43c13630c4a32462cb0ffcbf6df36c80891c3c79
FancyGuitarist <simf3010@gmail.com>
2023-02-01 17:43:44 -0500

    Ok la pour de vraie
    En micro m au lieu de m lol

--- D_1b.py
+++ D_1b.py
@@ -2,23 +2,33 @@ import numpy as np
 import scipy.constants as sp
 import matplotlib.pyplot as plt
 
-lama1 = 1*10**(-6)
-lama2 = 1.6*10**(-6)
-lamb = np.arange(lama1,lama2,0.01*10**(-6))
+lama1 = 1
+lama2 = 1.6
+lamb = np.arange(lama1,lama2,0.01)
 A1 = 0.696166
-lamb1 = 0.068404*10**(-6)
+lamb1 = 0.068404
 A2 = 0.407942
-lamb2 = 0.116241*10**(-6)
+lamb2 = 0.116241
 A3 = 0.897479
-lamb3 = 9.896161*10**(-6)
+lamb3 = 9.896161
 A = [A1,A2,A3]
 Lamb = [lamb1,lamb2,lamb3]
-dn2 = A[0]*Lamb[0]**2*(Lamb[0]+3*lamb**3)+A[1]*Lamb[1]**2*(Lamb[1]+3*lamb**3)+A[2]*Lamb[2]**2*(Lamb[2]+3*lamb**3)
+dn2 = (A1*(lamb1**2)*((lamb1**2)+3*(lamb**3)))/(((lamb**2)-(lamb1**2))**3) + (A2*(lamb2**2)*((lamb2**2)+3*(lamb**3)))/(((lamb**2)-(lamb2**2))**3) + (A3*(lamb3**2)*((lamb3**2)+3*(lamb**3)))/(((lamb**2)-(lamb3**2))**3)
 
 #print(dn2)
-D = (-lamb/sp.c)*dn2*0.5
+D = (-lamb/(sp.c*10**(6)))*dn2
+print(sp.c)
+print(dn2)
 print(D)
-plt.plot(lamb,D)
-plt.show()
-#print(2*A[0]*lamb/(lamb**2-Lamb[0]**2)**2)
-#print(2*A[0]*lamb/((lamb**2-Lamb[0]**2)**2))
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(lamb, D, color = 'black')
+fig.tight_layout()
+fig.subplots_adjust(top=0.8)
+plt.xlabel('$\lambda$($\mu$m)')
+plt.ylabel('D')
+ax.tick_params(axis="y",direction="in")
+ax.tick_params(axis="x",direction="in")
+fig.savefig("1B_Optique", bbox_inches='tight',dpi=600)
+#print(2*A1*lamb/(lamb**2-Lamb[0]**2)**2)
+#print(2*A1*lamb/((lamb**2-Lamb[0]**2)**2))
