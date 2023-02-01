c7ad95552c12c6e34ee1d5fb329f396837ef5bc6
FancyGuitarist <simf3010@gmail.com>
2023-02-01 17:00:49 -0500

    Je penses que le 1b marche lala
    Please god

--- D_1b.py
+++ D_1b.py
@@ -1,17 +1,24 @@
 import numpy as np
 import scipy.constants as sp
+import matplotlib.pyplot as plt
 
-lamb = 1.6*(10**(-6))
+lama1 = 1*10**(-6)
+lama2 = 1.6*10**(-6)
+lamb = np.arange(lama1,lama2,0.01*10**(-6))
 A1 = 0.696166
-lamb1 = 0.068404*(10**(-6))
+lamb1 = 0.068404*10**(-6)
 A2 = 0.407942
-lamb2 = 0.116241*(10**(-6))
+lamb2 = 0.116241*10**(-6)
 A3 = 0.897479
-lamb3 = 9.896161*(10**(-6))
+lamb3 = 9.896161*10**(-6)
 A = [A1,A2,A3]
 Lamb = [lamb1,lamb2,lamb3]
-D = 0
-for i in range(2):
-    D += (1+((2*A[i])/(lamb**2-Lamb[i]**2))-((A[i]*(4*lamb-1))/4*lamb))**(-3/2)
-D = D*(lamb/(4*sp.c))
+dn2 = A[0]*Lamb[0]**2*(Lamb[0]+3*lamb**3)+A[1]*Lamb[1]**2*(Lamb[1]+3*lamb**3)+A[2]*Lamb[2]**2*(Lamb[2]+3*lamb**3)
+
+#print(dn2)
+D = (-lamb/sp.c)*dn2*0.5
 print(D)
+plt.plot(lamb,D)
+plt.show()
+#print(2*A[0]*lamb/(lamb**2-Lamb[0]**2)**2)
+#print(2*A[0]*lamb/((lamb**2-Lamb[0]**2)**2))
